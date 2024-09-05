efc0c31507d77577aab654e0f8b02fa178931f34
Stanislav <stanislavstranger@gmail.com>
2024-09-05 17:12:54 +0300

    feat: configure MongoDB connection and environment variables
    - Added environment variables for MongoDB connection (login, password, host, port, database, auth).
    - Configured `MongooseModule` with dynamic connection string using `ConfigService` for MongoDB.
    - Set up `getMongoConfig` function to dynamically generate MongoDB connection URI from env.
    - Integrated `MongooseModule` and `UserModule` in the main `AppModule`.
    - Registered `UserRepository`, `UserService`, and `UserController` in `UserModule`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './user/configs/mongo.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    MongooseModule.forRootAsync(getMongoConfig()),
+  ],
   controllers: [],
   providers: [],
 })

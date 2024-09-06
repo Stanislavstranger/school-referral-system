a5d32a78f5afa20217d88518115a44610092e4d1
Stanislav <stanislavstranger@gmail.com>
2024-09-06 18:24:16 +0300

    feat: add user entity with logic for managing lessons, passwords, and referrals
    - Added `UserEntity` to handle user-related operations, including:
      - lesson management
      - password validation.
    - Added method to generate referral code and update profile.

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,131 @@
+import { Exclude, Transform } from 'class-transformer';
+import { SUCH_A_RESERVATION_ALREADY_EXISTS } from '../constants/user.constants';
+import {
+  IUser,
+  UserRole,
+  IUserLessons,
+  PurchaseState,
+} from '../interfaces/user.interface';
+import { compare, genSalt, hash } from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
+
+export class UserEntity implements IUser {
+  _id?: unknown;
+  displayName: string;
+  firstName: string;
+  lastName: string;
+  patronymic?: string;
+  email: string;
+  phone: string;
+  @Exclude()
+  passwordHash: string;
+  referralCode: string;
+  invitedStudents?: string[];
+  role: UserRole;
+  lessons?: IUserLessons[];
+  version: number;
+  @Transform(({ value }) => new Date(value).getTime())
+  createdAt: number;
+  @Transform(({ value }) => new Date(value).getTime())
+  updatedAt: number;
+
+  constructor(
+    user: Omit<
+      IUser,
+      'passwordHash' | 'referralCode' | 'version' | 'createdAt' | 'updatedAt'
+    >,
+  );
+  constructor(user: IUser);
+
+  constructor(
+    user:
+      | IUser
+      | Omit<IUser, 'passwordHash' | 'version' | 'createdAt' | 'updatedAt'>,
+  ) {
+    this._id = user._id;
+    this.displayName = user.displayName;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.patronymic = user.patronymic || '';
+    this.email = user.email;
+    this.phone = user.phone;
+    this.role = user.role || UserRole.Student;
+    this.lessons = user.lessons || [];
+    this.referralCode = user.referralCode || this.generateReferralCode();
+    this.invitedStudents = user.invitedStudents || [];
+    this.version = 1;
+    this.createdAt = Date.now();
+    this.updatedAt = Date.now();
+
+    if ('passwordHash' in user) {
+      this.passwordHash = user.passwordHash;
+    }
+  }
+
+  private async isLessonExist(
+    lessonId: string,
+  ): Promise<IUserLessons | undefined> {
+    return this.lessons.find((lesson) => lesson.lessonId === lessonId);
+  }
+
+  public async addLesson(lessonId: string) {
+    if (this.isLessonExist(lessonId)) {
+      throw new Error(SUCH_A_RESERVATION_ALREADY_EXISTS);
+    }
+    this.lessons.push({
+      lessonId,
+      purchaseState: PurchaseState.Started,
+    });
+  }
+
+  public async deleteLesson(lessonId: string) {
+    this.lessons = this.lessons.filter(
+      (lesson) => lesson.lessonId !== lessonId,
+    );
+  }
+
+  getLessonState(lessonId: string): PurchaseState {
+    return (
+      this.lessons.find((lesson) => lesson.lessonId === lessonId)
+        ?.purchaseState ?? PurchaseState.Started
+    );
+  }
+
+  public async getPublicProfile(): Promise<
+    Omit<
+      IUser,
+      'passwordHash' | 'invitedStudents' | 'version' | 'createdAt' | 'updatedAt'
+    >
+  > {
+    return {
+      displayName: this.displayName,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      patronymic: this.patronymic,
+      email: this.email,
+      phone: this.phone,
+      referralCode: this.referralCode,
+      role: this.role,
+      lessons: this.lessons,
+    };
+  }
+
+  public async setPassword(password: string): Promise<this> {
+    const salt = await genSalt(10);
+    this.passwordHash = await hash(password, salt);
+    return this;
+  }
+
+  public async validatePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
+  }
+
+  public async updateProfile(invitedStudentId?: string): Promise<this> {
+    this.invitedStudents.push(invitedStudentId);
+    return this;
+  }
+
+  private generateReferralCode(): string {
+    return uuidv4();
+  }
+}

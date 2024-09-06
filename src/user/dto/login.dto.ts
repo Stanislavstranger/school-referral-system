ee279bea28b81336f69567f82a2c0009f6286f76
Stanislav <stanislavstranger@gmail.com>
2024-09-06 18:21:59 +0300

    feat: add DTOs for user registration and password update
    - Added `RegisterDto` for validating user registration data.
    - Added `UpdatePasswordDto` for validating password update requests.

---
+++ src/user/dto/login.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class Response {
+  access_token!: string;
+}
---
+++ src/user/dto/register.dto.ts
@@ -0,0 +1,43 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { UserRole } from '../interfaces/user.interface';
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  displayName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsString()
+  patronymic?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  referralCode?: string;
+
+  @IsNotEmpty()
+  @IsEnum(UserRole)
+  role: UserRole;
+}
+
+export class ResponseRegisterDto {
+  email: string;
+}

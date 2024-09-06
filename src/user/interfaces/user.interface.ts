4c4146979be8e8df4c93e26cb5a022b631793b72
Stanislav <stanislavstranger@gmail.com>
2024-09-06 18:25:41 +0300

    feat: add interfaces and enums for user roles and lesson states
    - Added `UserRole` enum to define roles (Teacher, Student).
    - Added `PurchaseState` enum to define lesson purchase states.
    - Added interfaces for users and lessons to standardize data structures.

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,34 @@
+export enum UserRole {
+  Teacher = 'Teacher',
+  Student = 'Student',
+}
+
+export enum PurchaseState {
+  Started = 'Started',
+  WaitingForPayment = 'WaitingForPayment',
+  Purchased = 'Purchased',
+  Canceled = 'Canceled',
+}
+
+export interface IUserLessons {
+  lessonId: string;
+  purchaseState: PurchaseState;
+}
+
+export interface IUser {
+  _id?: string | unknown;
+  displayName: string;
+  firstName: string;
+  lastName: string;
+  patronymic?: string;
+  email: string;
+  phone: string;
+  passwordHash: string;
+  referralCode: string;
+  invitedStudents?: string[];
+  role: UserRole;
+  lessons?: IUserLessons[];
+  version: number;
+  createdAt: number;
+  updatedAt: number;
+}

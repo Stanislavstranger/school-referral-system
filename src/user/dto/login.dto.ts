28635586524688c995a03ac498254fe6446725e7
Stanislav <stanislavstranger@gmail.com>
2024-09-07 18:50:16 +0300

    feat: add Swagger annotations to DTOs for user registration, login, and password update

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,15 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({
+    description: 'Email address of the student',
+    example: 'student@example.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: "Password for the student's account",
+    example: 'password123',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
 }
 
 export class Response {
+  @ApiProperty({
+    description: "Access token for the student's account",
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  })
   access_token!: string;
 }
--- src/user/dto/update-password.dto.ts
+++ src/user/dto/update-password.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePasswordDto {
+  @ApiProperty({
+    description: 'The current password of the user',
+    example: 'oldPassword123',
+  })
   @IsNotEmpty()
   @IsString()
   oldPassword: string;
 
+  @ApiProperty({
+    description: 'The new password to be set',
+    example: 'newPassword456',
+  })
   @IsNotEmpty()
   @IsString()
   newPassword: string;

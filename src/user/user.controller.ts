72f91c2c6591b134b53bb4beb9ea0dcb7f8379d4
Stanislav <stanislavstranger@gmail.com>
2024-09-07 18:47:48 +0300

    feat: add Swagger annotations to UserController for API documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,15 +10,35 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { RegisterDto } from './dto/register.dto';
+import { RegisterDto, ResponseRegisterDto } from './dto/register.dto';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiQuery,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { FindUserDto } from './dto/find-user.dto';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @UsePipes(new ValidationPipe())
   @Post()
-  @Post()
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiQuery({
+    name: 'referralCode',
+    required: false,
+    description: 'Referral code used for registration (optional)',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User successfully created',
+    type: ResponseRegisterDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request - Invalid data input' })
   create(
     @Body() createUserDto: RegisterDto,
     @Query('referralCode') referralCode?: string,
@@ -28,18 +48,40 @@ export class UserController {
 
   @UsePipes(new ValidationPipe())
   @Get()
+  @ApiOperation({ summary: 'Get a list of all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of users returned successfully',
+    type: [FindUserDto],
+  })
   findAll() {
     return this.userService.findAll();
   }
 
   @UsePipes(new ValidationPipe())
   @Get(':id')
+  @ApiOperation({ summary: 'Get a single user by ID' })
+  @ApiParam({ name: 'id', description: 'The ID of the user', type: String })
+  @ApiResponse({
+    status: 200,
+    description: 'User found successfully',
+    type: FindUserDto,
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
   @UsePipes(new ValidationPipe())
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user by ID' })
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the user to delete',
+    type: String,
+  })
+  @ApiResponse({ status: 200, description: 'User deleted successfully' })
+  @ApiResponse({ status: 404, description: 'User not found' })
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
   }

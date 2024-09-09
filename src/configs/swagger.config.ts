f7da4ce7f8e33e809904a8f0c6f8d13ed704d605
Stanislav <stanislavstranger@gmail.com>
2024-09-09 14:06:06 +0300

    refactor: move Swagger configuration to a centralized folder

---
+++ src/configs/swagger.config.ts
@@ -0,0 +1,15 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export const setupSwagger = (app: INestApplication): void => {
+  const config = new DocumentBuilder()
+    .setTitle('School Referral Program Management API')
+    .setDescription(
+      "This API provides functionality for managing a school's referral program. The API allows users to generate referral links, register students through invitations, process payments, and track statistics of referred students",
+    )
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};

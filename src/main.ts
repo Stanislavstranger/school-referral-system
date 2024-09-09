c699dfcd95457a3d373c8fa69bf8ed32f8c3d435
Stanislav <stanislavstranger@gmail.com>
2024-09-09 14:19:10 +0300

    feat: add AuthEntity and AuthInterface

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { setupSwagger } from './user/configs/swagger.config';
+import { setupSwagger } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
